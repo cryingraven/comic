9d68884c335916322980a1a6b79bac5859708c8c
Alan <103020761+cryingraven@users.noreply.github.com>
2025-05-15 19:31:50 +0700

    new release section

--- comic-web/src/app/page.tsx
+++ comic-web/src/app/page.tsx
@@ -1,22 +1,50 @@
+'use client'
+
 import AdsBanner from '@/components/ads/banner'
 import LatestBlogs from '@/components/blogs/latestblog'
 import ComicSlider from '@/components/hero/comics'
 import Carousel from '@/components/hero/slider'
 import DefaultBackendService from '@/services/default'
+import { useEffect, useState } from 'react'
 
-export default async function Home() {
-	const [topComics, banners, exclusiveComics] = await Promise.all([
-		DefaultBackendService.instance().getTopComics(0, 12),
-		DefaultBackendService.instance().getActiveBannerByPosition('HT'),
-		DefaultBackendService.instance().getComics(
-			'Exclusive',
-			undefined,
-			0,
-			12,
-			'views::desc'
-		),
-		DefaultBackendService.instance().getTopComics,
-	])
+export default function Home() {
+	const [topComics, setTopComics] = useState([])
+	const [banners, setBanners] = useState([])
+	const [exclusiveComics, setExclusiveComics] = useState([])
+	const [newReleaseComics, setNewReleaseComics] = useState([])
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const [
+				topComicsData,
+				bannersData,
+				exclusiveComicsData,
+				newReleaseComicsData,
+			] = await Promise.all([
+				DefaultBackendService.instance().getTopComics(0, 12),
+				DefaultBackendService.instance().getActiveBannerByPosition('HT'),
+				DefaultBackendService.instance().getComics(
+					'Exclusive',
+					undefined,
+					0,
+					12,
+					'views::desc'
+				),
+				DefaultBackendService.instance().getComics(
+					'New Release',
+					undefined,
+					0,
+					12,
+					'created_at::desc'
+				),
+			])
+			setTopComics(topComicsData)
+			setBanners(bannersData)
+			setExclusiveComics(exclusiveComicsData)
+			setNewReleaseComics(newReleaseComicsData)
+		}
+		fetchData()
+	}, [])
 
 	return (
 		<div className="flex flex-col mx-auto min-h-screen container">
@@ -25,6 +53,7 @@ export default async function Home() {
 			<ComicSlider title="Exclusive Comics" comics={exclusiveComics} />
 			<AdsBanner />
 			<LatestBlogs />
+			<ComicSlider title="New Release Comics" comics={newReleaseComics} />
 		</div>
 	)
 }
