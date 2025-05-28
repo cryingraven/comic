f33e99832ba66a5b66b4fa37e7ab4d6c65baf03e
Alan <103020761+cryingraven@users.noreply.github.com>
2025-05-28 21:37:18 +0700

    increate acquire

--- comic-backend/src/modules/app.module.ts
+++ comic-backend/src/modules/app.module.ts
@@ -37,8 +37,9 @@ import { CMSModule } from 'src/features/cms/cms.module';
         pool: {
           max: 10,
           min: 0,
-          acquire: 60000,
-          idle: 10000,
+          acquire: 120000,
+          idle: 30000,
+          evict: 20000,
         },
         dialectOptions: {
           connectTimeout: 60000,
