a854e31376ec14fd55b2d18068242c074942bf7d
Alan <103020761+cryingraven@users.noreply.github.com>
2025-05-29 18:04:22 +0700

    add next cache

--- comic-web/next.config.ts
+++ comic-web/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
 	/* config options here */
 	images: {
+		minimumCacheTTL: 300,
 		dangerouslyAllowSVG: true,
 		domains: [
 			'firebasestorage.googleapis.com',
