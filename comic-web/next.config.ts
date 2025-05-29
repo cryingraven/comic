278c54b83e0462892a69f0b80a70efd884bd23ba
Alan <103020761+cryingraven@users.noreply.github.com>
2025-05-29 18:17:37 +0700

    fix new config

--- comic-web/next.config.ts
+++ comic-web/next.config.ts
@@ -5,15 +5,55 @@ const nextConfig: NextConfig = {
 	images: {
 		minimumCacheTTL: 300,
 		dangerouslyAllowSVG: true,
-		domains: [
-			'firebasestorage.googleapis.com',
-			'images.pexels.com',
-			'via.placeholder.com',
-			'placehold.co',
-			'images.24comic.com',
-			'comic-images.alanmr.com',
-			'lh3.googleusercontent.com',
-			'link.storjshare.io',
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'firebasestorage.googleapis.com',
+				port: '', // Optional: Defaults to '' (standard ports 80/443)
+				pathname: '/**', // Allows any path on this hostname
+			},
+			{
+				protocol: 'https',
+				hostname: 'images.pexels.com',
+				port: '',
+				pathname: '/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'via.placeholder.com',
+				port: '',
+				pathname: '/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'placehold.co',
+				port: '',
+				pathname: '/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'images.24comic.com',
+				port: '',
+				pathname: '/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'comic-images.alanmr.com',
+				port: '',
+				pathname: '/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'lh3.googleusercontent.com',
+				port: '',
+				pathname: '/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'link.storjshare.io',
+				port: '',
+				pathname: '/**',
+			},
 		],
 	},
 	reactStrictMode: false,
